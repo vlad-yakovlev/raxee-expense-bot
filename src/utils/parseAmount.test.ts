909b0cc3cce436a02c4284c2d89aca891e0638ac
Vladislav Yakovlev <vlad309523@gmail.com>
2023-01-23 22:24:13 +0400

    feat: add comma support

--- src/utils/parseAmount.test.ts
+++ src/utils/parseAmount.test.ts
@@ -1,7 +1,8 @@
 import { parseAmount } from './parseAmount'
 
-it('should return parsedAmount', () => {
+it('should return parsed amount', () => {
   expect(parseAmount('1234.56789')).toBe(12345679)
   expect(parseAmount('1234.56')).toBe(12345600)
   expect(parseAmount('1234')).toBe(12340000)
+  expect(parseAmount('1234,56789')).toBe(12345679)
 })
